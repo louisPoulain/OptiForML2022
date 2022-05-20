d18c556197bfc5763c214538812bf20b5704c95a
eseiler <emilienseiler18@gmail.com>
2022-05-20 16:00:05 +0200

    add path management

---
+++ path.py
@@ -0,0 +1,8 @@
+import os
+
+
+# Directories paths
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+OUT_DIR = os.path.join(DIRNAME, 'output')
+TRAIN_HISTORY_DIR = os.path.join(OUT_DIR, 'training_history')
+TRAIN_MODEL_DIR = os.path.join(OUT_DIR, 'pretrained_model')
