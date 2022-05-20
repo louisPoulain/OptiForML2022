0fffe3e05cb764e76285984dd4d0a9f780a55e90
eseiler <emilienseiler18@gmail.com>
2022-05-20 19:05:32 +0200

    add figure path

--- path.py
+++ path.py
@@ -3,6 +3,9 @@ import os
 
 # Directories paths
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
+
 OUT_DIR = os.path.join(DIRNAME, 'output')
 TRAIN_HISTORY_DIR = os.path.join(OUT_DIR, 'training_history')
 TRAIN_MODEL_DIR = os.path.join(OUT_DIR, 'pretrained_model')
+
+FIGURE_DIR = os.path.join(DIRNAME, 'fig')
